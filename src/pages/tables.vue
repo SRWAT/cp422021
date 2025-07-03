443f5636cb5a551583177d637fd68e5e64d54fb5
unknown <siriwat.si@kkumail.com>
2025-07-03 15:15:13 +0700

    Initial commit

--- src/pages/tables.vue
+++ src/pages/tables.vue
@@ -1,53 +1,72 @@
 <script setup>
-import DemoSimpleTableBasics from '@/views/pages/tables/DemoSimpleTableBasics.vue'
-import DemoSimpleTableDensity from '@/views/pages/tables/DemoSimpleTableDensity.vue'
-import DemoSimpleTableFixedHeader from '@/views/pages/tables/DemoSimpleTableFixedHeader.vue'
-import DemoSimpleTableHeight from '@/views/pages/tables/DemoSimpleTableHeight.vue'
-import DemoSimpleTableTheme from '@/views/pages/tables/DemoSimpleTableTheme.vue'
+import InfoCard from "@/components/cards/InfoCard.vue";
 </script>
-
 <template>
-  <VRow>
-    <VCol cols="12">
-      <VCard title="Basic">
-        <DemoSimpleTableBasics />
-      </VCard>
+  <VCard>
+    <VCardItem>
+      <VCardTitle>โต๊ะในร้าน</VCardTitle>
+    </VCardItem>
+    <VCardText>
+      <VRow>
+        <VCol cols="3">
+          <InfoCard
+            title="โต๊ะทั้งหมด"
+            :stats="10"
+            unit="ตัว"
+            icon="mdi-table"
+            color="primary"
+          />
+        </VCol>
+        <VCol cols="3">
+          <InfoCard
+            title="โต๊ะว่าง"
+            :stats="5"
+            unit="ตัว"
+            icon="mdi-table-plus"
+            color="success"
+          />
+        </VCol>
+        <VCol cols="3">
+          <InfoCard
+            title="ใช้งานอยู่"
+            :stats="5"
+            unit="ตัว"
+            icon="mdi-table-account"
+            color="warning"
+          />
+        </VCol>
+        <VCol cols="3">
+          <VCard class="align-center justify-center d-flex fill-height">
+            <VBtn                
+              class="fill-height"
+              variant="text"
+              block
+              text
+            >
+              <VIcon>mdi-plus</VIcon>
+              เพิมโต๊ะใหม่
+            </VBtn>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VCardText>
+  </VCard>
+  <VCard class="mt-8">
+    <VCardText>
+<VRow>
+    <VCol cols="3" v-for="n in 10":key="n">
+        <VBtn
+          color="primary"
+          block
+         height="150"
+          class="text-h6"
+        >
+          <VIcon left class="mr-2" size="40">mdi-table-chair</VIcon>
+          โต๊ะที่ {{ n }}
+        </VBtn>
     </VCol>
-
-    <VCol cols="12">
-      <VCard title="Theme">
-        <VCardText>
-          use <code>theme</code> prop to switch table to the dark theme.
-        </VCardText>
-        <DemoSimpleTableTheme />
-      </VCard>
-    </VCol>
-
-    <VCol cols="12">
-      <VCard title="Density">
-        <VCardText>
-          You can show a dense version of the table by using the <code>density</code> prop.
-        </VCardText>
-        <DemoSimpleTableDensity />
-      </VCard>
-    </VCol>
-
-    <VCol cols="12">
-      <VCard title="Height">
-        <VCardText>
-          You can set the height of the table by using the <code>height</code> prop.
-        </VCardText>
-        <DemoSimpleTableHeight />
-      </VCard>
-    </VCol>
-
-    <VCol cols="12">
-      <VCard title="Fixed Header">
-        <VCardText>
-          You can fix the header of table by using the <code>fixed-header</code> prop.
-        </VCardText>
-        <DemoSimpleTableFixedHeader />
-      </VCard>
-    </VCol>
-  </VRow>
+            
+ </VRow>
+    </VCardText>
+  </VCard>
 </template>
